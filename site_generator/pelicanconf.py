bfac3489fcb8c13f9827af1069e68873e2be5928
Matt Jones <mjones@yieldbot.com>
2015-01-27 01:55:03 -0500

    Add site

---
+++ site_generator/pelicanconf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'varied'
+SITENAME = u'Sensu Plugins'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PYGMENTS_STYLE = 'monokai'
+BOOTSTRAP_FLUID = True
+
+BOOTSTRAP_NAVBAR_INVERSE = False
+DISPLAY_TAGS_ON_SIDEBAR = False
+THEME = './pelican-bootstrap3'
+
+# GITHUB_USER = 'mattyjones'
+# GITHUB_REPO_COUNT = 5
+# GITHUB_SKIP_FORK = False
+#GITHUB_SHOW_USER_LINK = False
+
+# Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+# SOCIAL = (('twitter', 'https://twitter.com/DevopsMatt'),
+#           ('linkedin', 'https://www.linkedin.com/in/mattyjones'),
+#           ('github', 'https://github.com/mattyjones'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
